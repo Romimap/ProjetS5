a33f711e529302d8f6e8c32afd269f856d2defa5
Romimap <romimap75@gmail.com>
2019-10-09 14:31:30 +0200

    Create template.php

---
+++ www/template/template.php
@@ -0,0 +1,11 @@
+<!-- PAGE TEMPLATE -->
+<!doctype html>
+<html lang="fr">
+  <?php include("/template/head.html"); ?>
+  <body>
+    
+    <!-- PAGE -->
+    
+    <?php include("/template/bootstrap_scripts.html"); ?>
+  </body>
+</html>
