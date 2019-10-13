823701cc8c6dffc1023028951bcb3cbc65cae505
Romain Fournier <romain.fournier.095@gmail.com>
2019-10-13 09:25:41 +0200

    added functions

--- www/template/creationCompte.php
+++ www/template/creationCompte.php
@@ -3,6 +3,57 @@
 # @Date: 12th of October, 2019
 # @Email: romain.fournier.095@gmail.com
 
+
+/**
+ * username regex: /^[a-z]{1}\w{4,19}$/i
+ * starts with a letter, then alphanumeric + underscore, 5-20 chars, case insensitive
+ */
+function isUsername ($str) {
+    if (!is_string($str))
+	return false;
+    if (!preg_match('/^[a-z]{1}\w{4, 14}$/i', $str))
+	return false;
+    return true;
+}
+
+/**
+ * name regex: /^[a-z]{2,20}$/i
+ * letters, 5-20 chars, case insensitive
+ */
+function isName ($str) {
+    if (!is_string($str))
+	return false;
+    if (!preg_match('/^[a-z]{2,20}$/i', $str))
+	return false;
+    return true;
+}
+
+/**
+ * password regex: /^[a-zA-Z0-9!@#._]{2,20}$/
+ * alphanumeric and !@#._    , 8-20 chars, case sensitive
+ */
+function isPassword ($str) {
+    if (!is_string($str))
+	return false;
+    if (!preg_match('/^[a-zA-Z0-9!@#._]{2,20}$/', $str))
+	return false;
+    return true;
+}
+
+/**
+ * mail regex: /^[a-z0-9._]{1,20}@{1}[a-z0-9._]{1,20}$/i
+ * alphanumeric + underscore + dot  @  alphanumeric + underscore + dot, 5-50 chars, case insensitive
+ */
+function isMail ($str) {
+    if (!is_string($str))
+	return false;
+    if (!preg_match('/^[a-z0-9._]{1,20}@{1}[a-z0-9._]{1,20}$/i', $str))
+	return false;
+    return true;
+}
+
+
+
 /**
  * Needs a token and a complete, valid form
  * token: $_POST['token']
@@ -13,28 +64,21 @@ if (isset($_POST['token'] && isset($_SESSION['tokenInstance'])) {
     //Token check
     if ($_SESSION['tokenInstance']->verify($_POST['token'])) {
 	//We check if the form is complete and valid (TODO: test it)
-	if (//Username, alphanumeric, starts with a letter
-	    isset($_POST['username']) &&
-	    preg_match("^[a-zA-Z]+[a-zA-Z0-9]*", $_POST['username']) &&
-	    //Password, alphanumeric
+	if (isset($_POST['username']) &&
+	    isUsername($_POST['username']) &&
 	    isset($_POST['password']) &&
-	    preg_match("[a-zA-Z0-9]*", $_POST['password']) &&
-	    //Password Confirmation, alphanumeric
+	    isPassword($_POST['password']) &&
 	    isset($_POST['passwordConfirmation']) &&
-	    preg_match("[a-zA-Z0-9]*", $_POST['passwordConfirmation']) &&
-	    //First name, Alphabetic
+	    isPassword($_POST['passwordConfirmation']) &&
 	    isset($_POST['firstName']) &&
-	    preg_match("[A-Za-z]*", $_POST['firstName']) &&
-	    //Last name, Alphabetic
+	    isName($_POST['firstName']) &&
 	    isset($_POST['lastName']) &&
-	    preg_match("[A-Za-z]*", $_POST['lastName']) &&
-	    //Email, Alphanumeric+(._)  @  Alphanumeric+(._)  .  Alphabetic
+	    isName($_POST['lastName']) &&
 	    isset($_POST['email']) &&
-	    preg_match("[A-Za-z0-9._]*+@+[A-Za-z0-9._]*+.+[a-z]*", $_POST['email']) &&
-	    //City, Alphabetic
-	    isset($_POST['city'] &&)
-	    preg_match("[A-Za-z]*", $_POST['city'])) {
-	    //From now on, we will consider that the form is complete, valid and
+	    isMail($_POST['email']) &&
+	    isset($_POST['city']) &&
+	    isName($_POST['city'])) {
+	    //From now on, we will consider that the form is complete, valid and\
 	    //was sent by the owner of the session
 	    
 	}
