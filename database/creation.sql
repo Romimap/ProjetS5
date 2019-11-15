78652e3475553db771c9912afb67069a8f92495b
Romimap <romimap75@gmail.com>
2019-11-15 16:06:59 +0100

    Add files via upload

--- database/creation.sql
+++ database/creation.sql
@@ -1,14 +1,14 @@
--------------------------------------------------------
+-- -----------------------------------------------------
 -- Groupe K
 -- Membres :
 --     - Benhammadi Youssef
 --     - Fournier Romain
--------------------------------------------------------
+-- -----------------------------------------------------
 
 
--------------------------------------------------------
+-- -----------------------------------------------------
 -- Création des tables et des contraintes
--------------------------------------------------------
+-- -----------------------------------------------------
 
 CREATE TABLE evenement (
   id INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
@@ -22,14 +22,12 @@ CREATE TABLE evenement (
   date_fin DATE NOT NULL,
   effectif_min INT(5) DEFAULT NULL,
   effectif_max INT(5) DEFAULT NULL,
-  --KEYS
+  -- KEYS
   PRIMARY KEY (id),
   KEY k_taxonomie (id_mot_clef),
-  KEY k_membre (id_membre),
-  --CONSTRAINS
-  CONSTRAINT fk_evenement_membre FOREIGN KEY (id_membre) REFERENCES membres (id),
-  CONSTRAINT fk_evenement_taxonomie FOREIGN KEY (id_mot_clef) REFERENCES taxonomie (id);
-)
+  KEY k_membre (id_membre)
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE inscriptions (
@@ -38,14 +36,12 @@ CREATE TABLE inscriptions (
   id_membre INT(11) NOT NULL,
   note TINYINT(4) NOT NULL DEFAULT 0,
   commentaire VARCHAR(240) DEFAULT NULL,
-  --KEYS
+  -- KEYS
   PRIMARY KEY (id),
   KEY fk_membre (id_membre),
-  KEY fk_evenement (id_evenement),
-  --CONSTRAINS
-  CONSTRAINT fk_inscription_evenement FOREIGN KEY (id_evenement) REFERENCES evenement (id),
-  CONSTRAINT fk_inscription_membre FOREIGN KEY (id_membre) REFERENCES membres (id);
-)
+  KEY fk_evenement (id_evenement)
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE membres (
   id INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
@@ -59,31 +55,48 @@ CREATE TABLE membres (
   telephone VARCHAR(10) DEFAULT NULL,
   role VARCHAR(20) NOT NULL DEFAULT 'Visiteur',
   ban DATE DEFAULT NULL,
-  --KEYS
+  -- KEYS
   PRIMARY KEY (id),
-  --CONSTRAINS
+  -- CONSTRAINTS
   CONSTRAINT role_check CHECK (role IN ('Visiteur', 'Contributeur', 'Admin')),
   CONSTRAINT email_check CHECK (email LIKE '%@%')
-)
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE photos (
   id INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
   id_evenement INT(11) NOT NULL,
   lien VARCHAR(240) NOT NULL,
-  --KEYS
+  -- KEYS
   PRIMARY KEY (id),
-  KEY fk (id_evenement),
-  --CONSTRAINS
-  CONSTRAINT fk_photos_evenement FOREIGN KEY (id_evenement) REFERENCES evenement (id);
-)
+  KEY fk (id_evenement)
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE taxonomie (
   id INT(11) NOT NULL UNIQUE AUTO_INCREMENT,
   parent INT(11) NOT NULL,
   mot VARCHAR(20) NOT NULL,
-  --KEYS
+  -- KEYS
   PRIMARY KEY (id),
-  KEY fk (parent),
-  --CONSTRAINS
-  CONSTRAINT fk_taxonomie_taxonomie FOREIGN KEY (parent) REFERENCES taxonomie (id);
-)
+  KEY fk (parent)
+  
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- -----------------------------------
+-- ajout des clefs etrangeres
+-- -----------------------------------
+
+ALTER TABLE evenement
+  ADD CONSTRAINT fk_evenement_membre FOREIGN KEY (id_membre) REFERENCES membres (id),
+  ADD CONSTRAINT fk_evenement_taxonomie FOREIGN KEY (id_mot_clef) REFERENCES taxonomie (id);
+  
+ALTER TABLE inscriptions
+  ADD CONSTRAINT fk_inscription_evenement FOREIGN KEY (id_evenement) REFERENCES evenement (id),
+  ADD CONSTRAINT fk_inscription_membre FOREIGN KEY (id_membre) REFERENCES membres (id);
+
+ALTER TABLE photos
+  ADD CONSTRAINT fk_photos_evenement FOREIGN KEY (id_evenement) REFERENCES evenement (id);
+  
+ALTER TABLE taxonomie
+  ADD CONSTRAINT fk_taxonomie_taxonomie FOREIGN KEY (parent) REFERENCES taxonomie (id);
