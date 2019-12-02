d32d55fc8a3376b4738ead67487179cfcae1274b
Romi <romain.fournier.095@gmail.com>
2019-12-02 17:58:09 +0100

    move

---
+++ www/template/deconnection.php
@@ -0,0 +1,8 @@
+<?php
+$WWWPATH = "/opt/lampp/htdocs/www/ProjetS5/www/";
+require($WWWPATH . "template/includes.php");
+//We delete the userToken cookie and reset the session
+setcookie('userToken', time() - 3600);
+session_destroy();
+session_start();
+?>
