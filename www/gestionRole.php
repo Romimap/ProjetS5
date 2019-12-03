2470dab78eaabf02e80c9b4c5a65455fbb53be63
Romi <romain.fournier.095@gmail.com>
2019-12-03 16:36:10 +0100

    la requete retourne les correspondances exactes en premier, + style

--- www/gestionRole.php
+++ www/gestionRole.php
@@ -22,9 +22,9 @@ if (isset($_SESSION['userInfo']['role']) && $_SESSION['userInfo']['role'] == "Ad
                 <form method="POST" action="gestionRole.php">
                     <div class="row pl-3 pr-3">
                         <div class="input-group col-3">
-                            <input type="text" class="form-control" placeholder="Username" name="nameSearch"  style="height: 20px;">
+                            <input type="text" class="form-control" placeholder="Username" name="nameSearch"  style="height: 40px;">
                             <div class="input-group-append">
-                                <input class="input-group-text" type="submit" value="Rechercher !"  style="height: 20px;">
+                                <input class="input-group-text" type="submit" value="Rechercher !"  style="height: 40px;">
                             </div>
                         </div>
                         <div class="col text-right">
@@ -38,26 +38,32 @@ if (isset($_SESSION['userInfo']['role']) && $_SESSION['userInfo']['role'] == "Ad
         </div>';
         if (isset($_POST['nameSearch']) && ctype_alnum($_POST['nameSearch'])) {
             //Display users
-            $prepared = $bdd->prepare("SELECT id, username, role FROM membres WHERE username LIKE :nameSearch");
-            $values = array(':nameSearch' => "%$_POST[nameSearch]%");
+            //First we get user data from the database, showing exact matchs first
+            $prepared = $bdd->prepare("SELECT id, username, role FROM membres
+                                        WHERE username LIKE :nameSearch
+                                        ORDER BY (username=:exactName) DESC, LENGTH(username)
+                                        LIMIT 50;");
+            $values = array(':nameSearch' => "%$_POST[nameSearch]%", ':exactName' => $_POST['nameSearch']);
             if ($prepared->execute($values)) {
+                //We executed the query, we can start to print the form
                 echo '
                     <form method="POST" action="gestionRole.php">
                         <div class="row">
                             <div class="col-4 border-right">
                                 <ul class="list-group border-0" style="overflow-y:  scroll; height: 500px;">';
+                                //Here we print the usernames on the left
                 while ($row = $prepared->fetch()) {
                     echo '
                                     <li class="list-group-item border-0">
                                         <div class="input-group">
                                             <div class="input-group-prepend">
-                                                <div class="input-group-text" style="height: 20px;">
+                                                <div class="input-group-text" style="height: 40px;">
                                                     <input type="checkbox" name="id[' . $row['id'] . '] value="0"/>
                                                 </div>
                                             </div>
-                                            <p class="form-control text-capitalize" style="height: 20px;">' . $row['username'] . '</p>
+                                            <p class="form-control text-capitalize" style="height: 40px;">' . $row['username'] . '</p>
                                             <div class="input-group-append">
-                                                <div class="input-group-text" style="height: 20px;">
+                                                <div class="input-group-text" style="height: 40px;">
                                                     <p>' . $row['role'] . '</p>
                                                 </div>
                                             </div>
@@ -66,7 +72,9 @@ if (isset($_SESSION['userInfo']['role']) && $_SESSION['userInfo']['role'] == "Ad
                 }
                 echo '
                                 </ul>
-                            </div>
+                            </div>';
+                            //Here we print the options and the submit button on the right
+                echo '
                             <div class="col-8">
                                 <div class="row pt-3 pb-2 border-bottom">
                                     <div class="col">
@@ -76,31 +84,31 @@ if (isset($_SESSION['userInfo']['role']) && $_SESSION['userInfo']['role'] == "Ad
                                 <div class="row pt-3">
                                     <div class="input-group mb-3 col-3">
                                         <div class="input-group-prepend">
-                                            <div class="input-group-text" style="height: 20px;">
+                                            <div class="input-group-text" style="height: 40px;">
                                                 <input type="radio" name="role" value="Visiteur" checked>
                                             </div>
                                         </div>
-                                        <p class="form-control" style="height: 20px;"> Visiteur </p>
+                                        <p class="form-control" style="height: 40px;"> Visiteur </p>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="input-group mb-3 col-3">
                                         <div class="input-group-prepend">
-                                            <div class="input-group-text" style="height: 20px;">
+                                            <div class="input-group-text" style="height: 40px;">
                                                 <input type="radio" name="role" value="Contributeur">
                                             </div>
                                         </div>
-                                        <p class="form-control" style="height: 20px;"> Contributeur </p>
+                                        <p class="form-control" style="height: 40px;"> Contributeur </p>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="input-group mb-3 col-3">
                                         <div class="input-group-prepend">
-                                            <div class="input-group-text" style="height: 20px;">
+                                            <div class="input-group-text" style="height: 40px;">
                                                 <input type="radio" name="role" value="Admin">
                                             </div>
                                         </div>
-                                        <p class="form-control" style="height: 20px;"> Admin </p>
+                                        <p class="form-control" style="height: 40px;"> Admin </p>
                                     </div>
                                 </div>
                                 <div class="row pt-3 pb-2 border-top">
