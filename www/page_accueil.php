adb228c5856ef69e13c34d638e47f4e16e526c47
Romain Fournier <romain.fournier.095@gmail.com>
2019-12-06 15:46:36 +0100

    rename + template

--- www/page_accueil.php
+++ www/page_accueil.php
@@ -1,65 +1,16 @@
+<!-- PAGE TEMPLATE -->
+<?php
+$WWWPATH = "/opt/lampp/htdocs/www/ProjetS5/www/";
+require($WWWPATH . "template/includes.php");
+?>
 <!doctype html>
-<html lang="en">
-  <head>
-    <link rel="stylesheet" type="text/css" href="css/stylespa.css" media="screen" />
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Hello, world!</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-  </head>
+<html lang="fr">
+    <head>
+	<?php include($WWWPATH . "template/head.html"); ?>
+	<!-- HEAD -->
+    </head>
   <body class="d-flex flex-column">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Projet HLIN510 & HLIN511</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!--ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Acceuil <span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Inscription</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Connexion</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Devenez vendeur</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Incri</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-              </li>-->
-            </ul>
-            <div class="container h-100">
-                <div class="d-flex flex-row-reverse">
-                    <div class="searchbar">
-                      <input class="search_input" type="text" name="" placeholder="Que cherchez-vous ?">
-                      <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
-                    </div>
-                  </div>
-                </div>
-          </div>
-    </nav>
-
-
-
+    <?php include($"")
     <div id="page-content">
       <div class="container text-center">
         <div class="row justify-content-center">
@@ -97,7 +48,7 @@
         <div class="carousel-item" style="background-image: url('https://www.numerama.com/content/uploads/2019/08/zevent-2019.jpg')">
           <div class="carousel-caption d-none d-md-block">
             <!--<h2 class="display-4">A la une</h2>
-            <p class="lead">Le vendeur n’est payé que lorsque la commande est terminée </p>-->  
+            <p class="lead">Le vendeur n’est payé que lorsque la commande est terminée </p>-->
           </div>
         </div>
       </div>
@@ -111,7 +62,7 @@
             </a>
       </div>
     </header>
-  
+
     <div id="page-content">
       <div class="container text-center">
         <div class="row justify-content-center">
@@ -130,7 +81,7 @@
         <!-- <p class="lead">The background images for the slider are set directly in the HTML using inline CSS. The images in this snippet are from <a href="https://unsplash.com">Unsplash</a>, taken by <a href="https://unsplash.com/@joannakosinska">Joanna Kosinska</a>!</p> -->
         <p class="lead">Pas de panique, il y en aura sûrement d'autres.</p>
       </div>
-    </section>  
+    </section>
     <!--<div class="container">
       <div class="row">
         <div class="col-xl-3 col-md-6 mb-4">
@@ -285,10 +236,7 @@
         <small>Copyright &copy; Projet HLIN510 & HLIN511 - 2019/2020</small>
       </div>
     </footer>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    <?php include($WWWPATH . "template/bootstrapScripts.html"); ?>
+</body>
 </html>
+<?php $_SESSION['token']->cycle(); ?>
