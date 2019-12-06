4adc609638b53c81decf02f454603d4e7d3eeedf
Romain Fournier <romain.fournier.095@gmail.com>
2019-12-06 19:18:04 +0100

    Modification menu admin

--- www/template/menu/menu.php
+++ www/template/menu/menu.php
@@ -2,6 +2,7 @@
     if (isset($_SESSION['userInfo']['role'])) {
         if ($_SESSION['userInfo']['role'] == "Admin") {
             //Menu Admin
+            include($WWWPATH . "template/menu/menuAdmin.html");
         } else if ($_SESSION['userInfo']['role'] == "Contributeur") {
             //Menu Contributeur
             include($WWWPATH . "template/menu/menuClean.html");
