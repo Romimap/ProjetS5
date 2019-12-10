54456581b2ced698c46866cdcc5395723b937f7b
Romi <romain.fournier.095@gmail.com>
2019-12-10 22:13:43 +0100

    script de désinscription

---
+++ www/template/unregister.php
@@ -0,0 +1,28 @@
+<?php
+require('includes.php');
+echo 'p';
+//We check if we have a token
+if (isset($_POST['token']) && isset($_SESSION['token'])) {
+    echo 'a';
+    //Token check
+    if ($_SESSION['token']->verify($_POST['token'])) {
+        echo 'b';
+        //we check if the form is valid
+        if (isset($_POST['id']) && is_numeric($_POST['id'])
+        && isset($_SESSION['userInfo']['id']) && is_numeric($_SESSION['userInfo']['id'])) {
+            //the form is safe
+            //We can now unregister the client
+            $prepared = $bdd->prepare("DELETE FROM inscriptions WHERE id_membre=:idm AND id_evenement=:ide");
+            $values = array(':idm' => $_SESSION['userInfo']['id'], ':ide' => $_POST['id']);
+            if ($prepared->execute($values)) {
+                //The user is now unregistered to the event
+                header('location: ../PageEvenement.php?id='.$_POST['id']);
+            } else {
+                //an error occured
+                header('location: ../PageEvenement.php?id='.$_POST['id']);
+            }
+        }
+    }
+}
+
+ ?>
