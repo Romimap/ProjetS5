7e06d3744892e41c5287466f624f7870fabe383d
Romi <romain.fournier.095@gmail.com>
2019-12-10 22:40:24 +0100

    Historique Visiteur

--- www/ProfilePage.php
+++ www/ProfilePage.php
@@ -210,7 +210,36 @@ require($WWWPATH . "template/includes.php");
                             </div>
                             <?php
                                 if (isset($_SESSION['userInfo']['id']) && $_GET['id'] == $_SESSION['userInfo']['id']) { ?>
-                                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+                                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                    <?php
+                                    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+                                        $prepared = $bdd->prepare("SELECT evenement.id AS eid, nom, date_debut, date_fin FROM inscriptions, evenement
+                                        WHERE inscriptions.id_evenement=evenement.id AND inscriptions.id_membre=:id");
+                                        $values = array(':id' => $_GET['id']);
+                                        if ($prepared->execute($values)) {
+                                            while ($row = $prepared->fetch()) {
+                                                setlocale(LC_ALL, 'fr_FR');
+                                                $dateDebut = strtotime($row['date_debut']);
+                                                $dateStr = strftime("%e %B", $dateDebut);
+                                                $dateFin = strtotime($row['date_fin']);
+                                                if ($row['date_fin'] != $row['date_debut'])
+                                                    $dateStr = "du " . $dateStr . " au " . strftime("%e %B", $dateFin);
+                                                echo '
+                                                <div class="col-12">
+                                                    <div class="card mt-3">
+                                                        <div class="card-body">
+                                                            <h4 class="card-title">'. $row['nom'] .'</h4>
+                                                            <p class="card-text text-muted text-right">'. $dateStr .'</p>
+                                                            <a href="PageEvenement.php?id='. $row['eid'] .'" class="stretched-link"></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                ';
+                                            }
+                                        }
+                                    }
+                                    ?>
+                                </div>
                                 <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
                             <?php }
                             ?>
