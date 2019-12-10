1ebe3e29361c85e43fa3786c5b40661d18fb39bb
Romi <romain.fournier.095@gmail.com>
2019-12-10 21:35:57 +0100

    base

---
+++ www/PageEvenement.php
@@ -0,0 +1,63 @@
+<!-- PAGE TEMPLATE -->
+<?php
+$WWWPATH = "/opt/lampp/htdocs/www/ProjetS5/www/";
+require($WWWPATH . "template/includes.php");
+?>
+<!doctype html>
+<html lang="fr">
+    <head>
+	<?php include($WWWPATH . "template/head.html"); ?>
+    <link rel="stylesheet" type="text/css" href="css/pageevent.css" media="screen" />
+  </head>
+     <body>
+        <?php include($WWWPATH . "template/menu/menu.php"); ?>
+        <?php
+        if (!(isset($_GET['id']) && is_numeric($_GET['id']))) {
+            //If we dont have an id to work with, we redirect
+            header('location: EventList.php');
+        }
+        $prepared = $bdd->prepare("SELECT membres.id as uid, username, evenement.nom, mot, description, email, telephone, adresse
+            FROM evenement, membres, taxonomie
+            WHERE evenement.id_membre = membres.id AND evenement.id_mot_clef = taxonomie.id
+            AND evenement.id = :id");
+        $values = array(':id' => $_GET['id']);
+        if ($prepared->execute($values)) {
+            if ($row = $prepared->fetch()) {
+                //The query executed and returner at least a row, we can display the page
+            } else {
+                header('location: EventList.php');
+            }
+        } else {
+            header('location: EventList.php');
+        }
+        ?>
+        <div class="container mt-5" id="container">
+            <h1 class="text-center"><?php echo $row['nom']; ?></h1>
+            <hr>
+            <div class="row">
+                <div class="col-sm-8">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11880.492291371422!2d12.4922309!3d41.8902102!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x28f1c82e908503c4!2sColosseo!5e0!3m2!1sit!2sit!4v1524815927977" width="100%" height="640" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </div>
+
+                <div class="col-sm-4" id="contact2">
+                    <h3><?php echo $row['mot']; ?></h3>
+                    <hr class="col-6">
+                    <br>
+                    <i class="fas fa-globe" style="color:#000"></i><?php echo $row['adresse']; ?><br>
+                    <br>
+                    <p><?php echo $row['description']; ?></p>
+                    <br>
+                    <hr class="col-6">
+                    <i class="fas fa-phone" style="color:#000"></i> <a href="ProfilePage.php?id=<?php echo $row['uid']; ?>"><?php echo $row['username']; ?></a><br>
+                    <br>
+                    <i class="fas fa-phone" style="color:#000"></i><?php echo $row['telephone']; ?><br>
+                    <i class="fa fa-envelope" style="color:#000"></i><?php echo $row['email']; ?><br>
+                    <br>
+                    <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">s'inscrire</button>
+                </div>
+            </div>
+        </div>
+        <?php include($WWWPATH . "template/bootstrapScripts.html"); ?>
+    </body>
+</html>
+<?php $_SESSION['token']->cycle(); ?>
